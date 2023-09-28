fb571c6d9705bbd419b2a9f0981193f08bf63cce
barbapapazes <e.soubiran25@gmail.com>
2023-09-28 23:46:40 +0200

    feat: add reload button

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,20 @@
 <script lang="ts" setup>
-const { data, refresh } = await useFetch('/api/urls')
+const { data, refresh, pending } = await useFetch('/api/urls')
 </script>
 
 <template>
   <UContainer as="section" class="py-4 flex flex-col gap-8">
     <h1 class="text-4xl text-gray-900 dark:text-gray-50 font-bold">
-      Keys
+      Shorts
     </h1>
 
     <ShortsForm @refresh="refresh" />
 
-    <ShortsTable v-if="data" :data="data" @refresh="refresh" />
+    <UButton @click="refresh" icon="i-heroicons-arrow-path" class="self-start" :loading="pending">
+      Refresh
+    </UButton>
+
+    <ShortsTable v-if="data" :data="data" @refresh="refresh" :loading="pending" />
+
   </UContainer>
 </template>
