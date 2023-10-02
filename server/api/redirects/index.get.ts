b55d62720e068f7233f660a0661c858fb52a7191
barbapapazes <e.soubiran25@gmail.com>
2023-10-02 23:20:19 +0200

    chore: try fixing cf

--- server/api/redirects/index.get.ts
+++ server/api/redirects/index.get.ts
@@ -1,5 +1,3 @@
-import { eq } from 'drizzle-orm'
-
 export default defineEventHandler(async (event) => {
   requireBasicAuth(event)
 
@@ -15,23 +13,23 @@ export default defineEventHandler(async (event) => {
   })
 
   // @see CF bug https://github.com/cloudflare/workers-sdk/issues/3160
-  const redirects = await useDb().select().from(tables.redirects).leftJoin(tables.activities, eq(tables.activities.redirectId, tables.redirects.id)).all()
+  // const redirects = await useDb().select().from(tables.redirects).leftJoin(tables.activities, eq(tables.activities.redirectId, tables.redirects.id)).all()
 
-  return redirects
+  // return redirects
 
-  const data = redirects.reduce((acc, { redirects, activities }) => {
-    const index = acc.findIndex(({ id }) => id === redirects.id)
-    const isNewItem = index === -1
+  // const data = redirects.reduce((acc, { redirects, activities }) => {
+  //   const index = acc.findIndex(({ id }) => id === redirects.id)
+  //   const isNewItem = index === -1
 
-    const { id, name, url } = redirects
+  //   const { id, name, url } = redirects
 
-    if (isNewItem)
-      acc.push({ id, name, url, countActivities: activities ? 1 : 0 })
-    else if (activities && !isNewItem)
-      acc[index].countActivities++
+  //   if (isNewItem)
+  //     acc.push({ id, name, url, countActivities: activities ? 1 : 0 })
+  //   else if (activities && !isNewItem)
+  //     acc[index].countActivities++
 
-    return acc
-  }, [] as { id: number; name: string; url: string; countActivities: number }[])
+  //   return acc
+  // }, [] as { id: number; name: string; url: string; countActivities: number }[])
 
-  return data
+  // return data
 })
